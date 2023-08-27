73501c65c67ff5582ecaf64110d7deedbfb3f997
paulagg <paulgamboa99@gmail.com>
2023-08-27 15:55:05 -0600

    Actividad 2

---
+++ _p05_OperacionesMatematicas.java
@@ -0,0 +1,22 @@
+//Realiza operaciones matematicas basicas
+
+public class _p05_OperacionesMatematicas {
+    public static void main(String[] args) {
+        double x, y, suma, resta, mult, div, res, pot;
+        x = 10.5 ;
+        y = 2.5 ;
+        suma = x + y ;
+        resta = x - y ;
+        mult = x * y ;
+        div = x / y;
+        res = x % y;
+        pot = Math.pow(x, y);
+
+        System.out.println(String.format("Los numeros son: %.2f, %.2f",x,y));
+        System.out.println(String.format("Suma: %.2f\nResta:", suma, resta));
+        System.out.println(String.format("Multiplicacion: %.2f\nDivision: %.2f",mult,div));
+        System.out.println(String.format("Residuo: %.2f\nPotencia: %.2f",res,pot));
+
+    }
+    
+}
