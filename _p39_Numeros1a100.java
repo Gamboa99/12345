e4812be60ba459dd0eaf6e6a3dddf8f27608288d
paulagg <paulgamboa99@gmail.com>
2023-09-06 22:28:01 -0600

    Actividad 7

---
+++ _p39_Numeros1a100.java
@@ -0,0 +1,16 @@
+// Imprime los numeros de del 1 al 100 usando ciclo while
+
+public class _p39_Numeros1a100{
+    public static void main(String[] args) {
+        int c;
+
+        c=1;
+
+        while( c <= 100 ){
+            System.out.print(c+" ");
+            c++;
+        }
+
+        System.out.println("\nProceso terminado ...");
+    }
+}
